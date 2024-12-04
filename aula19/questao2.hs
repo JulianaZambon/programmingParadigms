13c6104a8acc28f089539d22e99023e8daa5b0f2
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-12-04 18:26:43 -0300

    Atualizar o questao2.hs

--- aula19/questao2.hs
+++ aula19/questao2.hs
@@ -4,4 +4,13 @@
 -- crie uma funcao em haskell, com a respectiva definicao de tipo, que retorne o superfatorial 
 -- de um numero inteiro positivo N. poderao ser criadas funcoes auxiliares.
 
+
+-- Define a função que calcula o fatorial de um número
+fatorial :: Int -> Int
+fatorial 0 = 1
+fatorial n = n * fatorial (n - 1)
+
+-- Define a função superfatorial que calcula o produto dos fatoriais
 superfatorial :: Int -> Int
+superfatorial 0 = 1
+superfatorial n = fatorial n * superfatorial (n - 1)
