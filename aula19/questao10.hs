b6f3dde64f1e29f513bf7a85a578d9349d592e97
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-12-09 15:41:43 -0300

    Create questao10.hs

---
+++ aula19/questao10.hs
@@ -0,0 +1,4 @@
+-- Crie em Haskell a definicaoo de tipo para uma funcao que recebe um numero inteiro, duas listas de inteiros e
+-- retorna uma tupla com uma lista de inteiros e um numero inteiro
+
+funcao :: Int -> [Int] -> [Int] -> ([Int], Int)
