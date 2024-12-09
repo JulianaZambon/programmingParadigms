7e1cdca764dd4ba6b323580eb4c63e0cea20d631
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-12-09 14:03:48 -0300

    Atualizar o questao7.hs

--- aula19/questao7.hs
+++ aula19/questao7.hs
@@ -10,4 +10,21 @@
 (5, 23)
 --}
 
+-- Define o tipo da função
 goldbach :: Int -> (Int, Int)
+
+-- Implementação da função principal
+goldbach n
+  | n <= 2 || odd n = error "O número deve ser par e maior que 2, conforme a conjectura de Goldbach"
+  -- Gera a primeira tupla (x, y) de números primos menores que n, onde x + y = n
+  | otherwise = head [(x, y) | x <- primosAte (n - 2), y <- primosAte (n - 2), x + y == n, x /= y]
+
+-- Função auxiliar para verificar se um número é primo
+ehPrimo :: Int -> Bool
+ehPrimo 1 = False -- 1 não é primo
+ehPrimo 2 = True  -- 2 é primo
+ehPrimo x = all (\k -> x `mod` k /= 0) [2 .. floor (sqrt (fromIntegral x))]
+
+-- Função auxiliar para gerar todos os números primos menores que um dado número
+primosAte :: Int -> [Int]
+primosAte m = filter ehPrimo [2..m]
