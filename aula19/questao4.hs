9aa1807ee82ab8423f8205cc6a54134db7a02e08
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-12-09 13:25:41 -0300

    Atualizar o questao4.hs

--- aula19/questao4.hs
+++ aula19/questao4.hs
@@ -6,4 +6,12 @@
 10111
 --}
 
+-- Define o tipo da função
 binario :: Int -> Int
+
+-- Caso base: se o número for 0, retorna 0
+binario 0 = 0
+
+-- Passo recursivo: calcula o resto da divisão por 2 (último dígito binário)
+-- e chama a função para o quociente, deslocando os dígitos binários à esquerda
+binario n = binario (n `div` 2) * 10 + (n `mod` 2)
